2c885b722fa749772148aac407254acb8bdf212e
Larisa test <alexandrova.uu@yandex.ru>
2023-12-09 16:45:08 +0300

    change marmotmvn

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymi;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
